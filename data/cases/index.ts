2dbee21dbed1ad05d13e61631855db6573abcb03
Dom-33 <domnita.tudor@gmail.com>
2026-09-13 22:56:21 +0300

    Add Swarnlata Mishra to validated corpus

--- data/cases/index.ts
+++ data/cases/index.ts
@@ -1,8 +1,10 @@
 ﻿import type { ValidatedCaseRecord } from "@/lib/schema";
 import jamesLeininger from "./james-leininger-james-m-huston-jr.json";
 import hanumantSaxena from "./hanumant-saxena-maha-ram-singh.json";
+import swarnlataMishra from "./swarnlata-mishra-biya-pathak.json";
 
 export const validatedCases: ValidatedCaseRecord[] = [
   jamesLeininger as ValidatedCaseRecord,
   hanumantSaxena as ValidatedCaseRecord,
+  swarnlataMishra as ValidatedCaseRecord,
 ];
