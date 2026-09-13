f346074b204cc4ca696712f52664f04cb4ae3c06
Dom-33 <domnita.tudor@gmail.com>
2026-09-13 22:40:12 +0300

    Add Hanumant Saxena to validated corpus

--- data/cases/index.ts
+++ data/cases/index.ts
@@ -1,6 +1,8 @@
 ﻿import type { ValidatedCaseRecord } from "@/lib/schema";
 import jamesLeininger from "./james-leininger-james-m-huston-jr.json";
+import hanumantSaxena from "./hanumant-saxena-maha-ram-singh.json";
 
 export const validatedCases: ValidatedCaseRecord[] = [
   jamesLeininger as ValidatedCaseRecord,
+  hanumantSaxena as ValidatedCaseRecord,
 ];
