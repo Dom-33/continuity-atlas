f505a249d23e77c47834b0c36a3181b1b34c1df9
Dom-33 <domnita.tudor@gmail.com>
2026-09-14 20:13:59 +0300

    Add Purnima Ekanayake to validated corpus

--- data/cases/index.ts
+++ data/cases/index.ts
@@ -4,6 +4,7 @@ import hanumantSaxena from "./hanumant-saxena-maha-ram-singh.json";
 import swarnlataMishra from "./swarnlata-mishra-biya-pathak.json";
 import matiasDeStefano from "./mati-as-de-stefano-autobiographical-past-life-and-cosmic-memory-claims.json";
 import shantiDevi from "./shanti-devi-lugdi-devi.json";
+import purnimaEkanayake from "./purnima-ekanayake-jinadasa-perera.json";
 
 export const validatedCases: ValidatedCaseRecord[] = [
   jamesLeininger as ValidatedCaseRecord,
@@ -11,4 +12,5 @@ export const validatedCases: ValidatedCaseRecord[] = [
   swarnlataMishra as ValidatedCaseRecord,
   matiasDeStefano as ValidatedCaseRecord,
   shantiDevi as ValidatedCaseRecord,
+  purnimaEkanayake as ValidatedCaseRecord,
 ];
