150fd630832b56ad8763f01ae4e901db32a96dca
Dom-33 <domnita.tudor@gmail.com>
2026-09-14 18:47:05 +0300

    Add Shanti Devi to validated corpus

--- data/cases/index.ts
+++ data/cases/index.ts
@@ -3,10 +3,12 @@ import jamesLeininger from "./james-leininger-james-m-huston-jr.json";
 import hanumantSaxena from "./hanumant-saxena-maha-ram-singh.json";
 import swarnlataMishra from "./swarnlata-mishra-biya-pathak.json";
 import matiasDeStefano from "./mati-as-de-stefano-autobiographical-past-life-and-cosmic-memory-claims.json";
+import shantiDevi from "./shanti-devi-lugdi-devi.json";
 
 export const validatedCases: ValidatedCaseRecord[] = [
   jamesLeininger as ValidatedCaseRecord,
   hanumantSaxena as ValidatedCaseRecord,
   swarnlataMishra as ValidatedCaseRecord,
   matiasDeStefano as ValidatedCaseRecord,
+  shantiDevi as ValidatedCaseRecord,
 ];
