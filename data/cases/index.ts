5800339a692afe9f4fb7af690159c48e9ccee9c7
Dom-33 <domnita.tudor@gmail.com>
2026-09-14 17:49:55 +0300

    Add Matias De Stefano to validated corpus

--- data/cases/index.ts
+++ data/cases/index.ts
@@ -2,9 +2,11 @@
 import jamesLeininger from "./james-leininger-james-m-huston-jr.json";
 import hanumantSaxena from "./hanumant-saxena-maha-ram-singh.json";
 import swarnlataMishra from "./swarnlata-mishra-biya-pathak.json";
+import matiasDeStefano from "./mati-as-de-stefano-autobiographical-past-life-and-cosmic-memory-claims.json";
 
 export const validatedCases: ValidatedCaseRecord[] = [
   jamesLeininger as ValidatedCaseRecord,
   hanumantSaxena as ValidatedCaseRecord,
   swarnlataMishra as ValidatedCaseRecord,
+  matiasDeStefano as ValidatedCaseRecord,
 ];
